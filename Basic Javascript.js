b904a87f2c3c91930044342135de80457ca6a5b1
Zachary Boggs <55256124+GrimRipper14@users.noreply.github.com>
2026-02-17 09:54:02 -0500

    Update Basic Javascript.js

--- Basic Javascript.js
+++ Basic Javascript.js
@@ -1,1 +1,29 @@
+let counter = 0;
 
+function tickup(){
+  counter++
+   document.getElementById("counter").textContent = counter;
+}
+
+function tickdown(){
+  counter--
+   document.getElementById("counter").textContent = counter;
+}
+
+function runforloop(){
+  let output = "";
+  for(let i = 0; i <= counter, i++){
+    output =+ i + "";
+  }
+   document.getElementById("forLoopResult").textContent = counter;
+}
+
+function showoddnumbers(){
+  let output = "";
+  for(let i = 1; i<= counter; i++){
+    if (i % 2 !== 0){
+      outuput =+ i + "";
+    }
+  }
+   document.getElementById("oddNumberResult").textContent = counter;
+}
